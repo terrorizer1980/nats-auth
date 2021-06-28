f03bb19cb2af78d84bd0ccc9cec43e7f12a4ea4d
Rahul Sethuram <rahul@sethuram.com>
2021-06-28 19:07:39 +0300

    Use file

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,31 @@
-const publicKey = process.env.VECTOR_JWT_SIGNER_PUBLIC_KEY?.replace(/\\n/g, "\n");
+import { readFileSync } from "fs";
+
+let publicKey: string | undefined;
+if (process.env.VECTOR_JWT_SIGNER_PUBLIC_KEY_PATH) {
+  publicKey = readFileSync(
+    process.env.VECTOR_JWT_SIGNER_PUBLIC_KEY_PATH,
+    "utf-8"
+  );
+}
+publicKey = process.env.VECTOR_JWT_SIGNER_PUBLIC_KEY?.replace(/\\n/g, "\n");
 if (!publicKey) {
-  throw new Error(`VECTOR_JWT_SIGNER_PUBLIC_KEY is required`);
+  throw new Error(
+    `VECTOR_JWT_SIGNER_PUBLIC_KEY or VECTOR_JWT_SIGNER_PUBLIC_KEY_PATH is required`
+  );
 }
 
-const privateKey = process.env.VECTOR_JWT_SIGNER_PRIVATE_KEY?.replace(/\\n/g, "\n");
+let privateKey: string | undefined;
+if (process.env.VECTOR_JWT_SIGNER_PRIVATE_KEY_PATH) {
+  publicKey = readFileSync(
+    process.env.VECTOR_JWT_SIGNER_PRIVATE_KEY_PATH,
+    "utf-8"
+  );
+}
+privateKey = process.env.VECTOR_JWT_SIGNER_PRIVATE_KEY?.replace(/\\n/g, "\n");
 if (!privateKey) {
-  throw new Error(`VECTOR_JWT_SIGNER_PRIVATE_KEY is required`);
+  throw new Error(
+    `VECTOR_JWT_SIGNER_PRIVATE_KEY or VECTOR_JWT_SIGNER_PRIVATE_KEY_PATH is required`
+  );
 }
 
 const natsServers = process.env.VECTOR_NATS_URL;
